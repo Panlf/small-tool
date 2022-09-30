18fbf9323f7e330574173a16974fa98d0eb2fd94
Panlf <liangfeng_pan@163.com>
2022-09-30 21:27:41 +0800

    重新整理架构

---
+++ tool-odps/src/main/java/com/plf/tool/odps/dto/ColumnBaseInfo.java
@@ -0,0 +1,14 @@
+package com.plf.tool.odps.dto;
+
+import lombok.Data;
+
+@Data
+public class ColumnBaseInfo {
+    //字段名称
+    private String columnName;
+    //字段类型
+    private String type;
+    //字段注释
+    private String comment;
+}
+
---
+++ tool-odps/src/main/java/com/plf/tool/odps/dto/TableInfo.java
@@ -0,0 +1,14 @@
+package com.plf.tool.odps.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TableInfo {
+    private String tableName;
+    private String tableComment;
+    private Integer type; // 真实表 1 视图 10
+    private String project;
+    private List<ColumnBaseInfo> columnBaseInfoList;
+}
