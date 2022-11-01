8e86faf33464df4d1cee6c9c111f74922813ff33
panlf <1805893180@qq.com>
2022-11-01 19:30:26 +0800

    excel导出

--- tool-common/src/main/java/com/plf/tool/common/file/ExcelUtils.java
+++ tool-common/src/main/java/com/plf/tool/common/file/ExcelUtils.java
@@ -1,8 +1,11 @@
 package com.plf.tool.common.file;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 
 import java.util.List;
 import java.util.Map;
@@ -19,4 +22,38 @@ public class ExcelUtils {
         ExcelReader reader = ExcelUtil.getReader(FileUtil.file(filePath), sheet);
         return reader.readAll();
     }
+
+    /**
+     * 导出Excel文件
+     * @param excelPath 输出地址
+     * @param data 数据
+     * @param alias 别名
+     * @param title 标题行
+     * @param mergeColumn 标题行需要合并的列数
+     */
+    public  static <T>  void  exportExcel(String excelPath,
+                                          List<T> data,
+                                          Map<String,String> alias,
+                                          String title,
+                                          int mergeColumn){
+        ExcelWriter writer = ExcelUtil.getWriter(excelPath);
+        writer.setOnlyAlias(true);
+
+        if(CollectionUtil.isNotEmpty(alias)) {
+            alias.forEach((k, v) -> {
+                writer.addHeaderAlias(k, v);
+            });
+        }
+
+        if(StrUtil.isNotEmpty(title)) {
+            // 合并单元格后的标题行，使用默认标题样式
+            writer.merge(mergeColumn, title);
+        }
+
+        // 一次性写出内容，使用默认样式，强制输出标题
+        writer.write(data, true);
+        // 关闭writer，释放内存
+        writer.close();
+    }
+
 }
