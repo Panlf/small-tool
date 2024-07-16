e9670833e5d04232335b162e07b07d9fb29611bd
panlf <1805893180@qq.com>
2024-07-16 17:12:04 +0800

    调整细节

--- tool-common/src/main/java/com/plf/tool/common/jieba/DemoJieBa.java
+++ tool-common/src/main/java/com/plf/tool/common/jieba/DemoJieBa.java
@@ -25,24 +25,26 @@ import java.util.stream.Collectors;
  */
 public class DemoJieBa {
     public static void main(String[] args) throws IOException {
+        System.out.println(participleWord1("我不喜欢你"));
+
         /*System.out.println(
         cosineSimilarity(
                 new double[]{1,1,2,1,1,1,0,0,0}
                 ,new double[]{1,1,1,0,1,1,1,1,1}));
          */
-        String s1 = "我有点喜欢你";
-        String s2 = "我不喜欢你";
-        List<String> s1List = participleWord(s1);
-        List<String> s2List = participleWord(s2);
-        List<String> mergeList = mergeList(s1List,s2List);
+        //String s1 = "我有点喜欢你";
+        //String s2 = "我不喜欢你";
+        //List<String> s1List = participleWord(s1);
+        //List<String> s2List = participleWord(s2);
+        //List<String> mergeList = mergeList(s1List,s2List);
         //System.out.println(participleWord(s2));
         //System.out.println(mergeList(s1List,s2List));
-        double[] s1Double = termFrequency(mergeList,s1List);
-        double[] s2Double = termFrequency(mergeList,s2List);
-        System.out.println(
-                cosineSimilarity(s1Double,s2Double));
+        //double[] s1Double = termFrequency(mergeList,s1List);
+        //double[] s2Double = termFrequency(mergeList,s2List);
+        //System.out.println(
+        //        cosineSimilarity(s1Double,s2Double));
 
-        System.out.println(jaccard(s1List,s2List));
+        //System.out.println(jaccard(s1List,s2List));
     }
 
     public void filterWord(){
@@ -80,6 +82,11 @@ public class DemoJieBa {
         JiebaSegmenter jiebaSegmenter = new JiebaSegmenter();
         List<SegToken> process = jiebaSegmenter.process(text, JiebaSegmenter.SegMode.INDEX);
         return process.stream().map(x->x.word).collect(Collectors.toList());
+    }
+
+    public static List<String> participleWord1(String text){
+        JiebaSegmenter jiebaSegmenter = new JiebaSegmenter();
+        return jiebaSegmenter.sentenceProcess(text);
     }
 
     public static double cosineSimilarity(double[] a,double[] b){
